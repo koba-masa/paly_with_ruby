9bba4b8a5571f0d794f4398840d664d95e0cb43e
koba-masa <kobayashi_mshr@koba-masa.com>
2022-07-10 17:34:11 +0900

    feat: ネストしたデータの定義方法の検証

--- spec/factories/factory_bot_tester.rb
+++ spec/factories/factory_bot_tester.rb
@@ -2,10 +2,61 @@ FactoryBot.define do
   factory :factory_bot_tester, class: 'FactoryBotTester' do
     first_name { 'John' }
     last_name { 'Doe' }
+    birthplace_prefcture { 'Saitama' }
+    birthplace_city { 'Saitama' }
 
     trait :yamada do
       last_name { 'Yamada' }
     end
+
+    trait :kanagawa do
+      birthplace_prefcture { 'Kanagawa' }
+      birthplace_city { 'Yokohama' }
+    end
+
+    # やりたいこと
+    #   呼び出し側で複数traitを指定してデータを構築するのではなく、
+    #   FactoryBot側で構築したものを返却したい。
+    #   かつ、部品化したい。
+    #   さらに、その子
+
+    ## 方法1：traitをネストさせる
+    ## 結果 ：子のtraitは定義されない
+    ##        => KeyError: Trait not registered: "sato"
+    trait :okinawa do
+      birthplace_prefcture { 'Okinawa' }
+      birthplace_city { 'Naha' }
+
+      trait :sato do
+        first_name { 'Sugar' }
+        last_name { 'Sato' }
+      end
+    end
+
+    ## 方法2
+    trait :suzuki do
+      kanagawa
+      first_name { 'Siro' }
+      last_name { 'Suzuki' }
+    end
+
+    ## 方法3
+    factory :hokkaido do
+      trait :tanaka do
+        first_name { 'Taro' }
+        last_name { 'Tanaka' }
+      end
+    end
+
+    ## 方法3
+    tarit :aomori do
+      factory :yamamoto do
+        first_name { 'Eiji' }
+        last_name { 'Yamamoto' }
+      end
+    end
+
+  end
 
   # traitの記載位置
   #   factroyブロックの外に定義した場合、他のFactoryにて同じシンボルが定義されているとエラーとなる
--- spec/models/factory_bot_tester_spec.rb
+++ spec/models/factory_bot_tester_spec.rb
@@ -5,6 +5,10 @@ require './spec/factories/factory_bot_trait_tester'
 RSpec.describe FactoryBotTester, type: :model do
   let(:john_doe) { FactoryBot.build(:factory_bot_tester) }
   let(:yamada) { FactoryBot.build(:factory_bot_tester, :yamada) }
+  let(:sato) { FactoryBot.build(:factory_bot_tester, :sato) }
+  let(:suzuki) { FactoryBot.build(:factory_bot_tester, :suzuki) }
+  let(:tanaka) { FactoryBot.build(:factory_bot_tester, :tanaka) }
+  let(:yamamoto) { FactoryBot.build(:factory_bot_tester, :yamamoto) }
 
   describe 'full_name' do
     it 'first_name last_nameの形式にて文字列を返却すること' do
@@ -13,4 +17,12 @@ RSpec.describe FactoryBotTester, type: :model do
     end
   end
 
+  describe 'self_introduce' do
+    it '自己紹介を行うこと' do
+      #expect(sato.self_introduce()).to eq('My name is Sugar Sato. I\'m from Okinawa Naha.')
+      expect(suzuki.self_introduce()).to eq('My name is Siro Suzuki. I\'m from Kanagawa Yokohama.')
+      #expect(tanaka.self_introduce()).to eq('My name is Taro Tanaka. I\'m from Hokkaido Sapporo.')
+      expect(yamamoto.self_introduce()).to eq('My name is Eiji Yamamoto. I\'m from Hokkaido Sapporo.')
+    end
+  end
 end
